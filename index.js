215575b41029dd6995e527a4a4dcca47059750f3
Tmnmediatrading <77391225+Tmnmediatrading@users.noreply.github.com>
2023-06-17 23:15:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const logr = require('./bear')
 const app = express()
-const port = 3002
+const port = process.env.PORT || 3002;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
